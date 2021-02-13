757ba852dd611f9f2df4c4cf89f0abe286b31e6f
Muhammad Hidayat <muhidayat03@gmail.com>
2021-02-13 10:03:19 +0700

    add auto loop for youtube player

--- src/pages/home/YoutubeSection.js
+++ src/pages/home/YoutubeSection.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const YoutubeSection = () =>
   <Section>
     <YoutubeContainer>
-      <Youtube src={"https://www.youtube.com/embed/8ASnyL_fZxQ?&autoplay=1&mute=1&enablejsapi=1&loop=1"} />
+      <Youtube src={"https://www.youtube.com/embed/8ASnyL_fZxQ?&autoplay=1&mute=1&enablejsapi=1&loop=1&playlist=8ASnyL_fZxQ"} />
     </YoutubeContainer>
   </Section>
 
