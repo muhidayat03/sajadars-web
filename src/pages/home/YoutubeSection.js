2d8214fbe57e8d1b76b20229b1ad2dcd60621b17
Muhammad Hidayat <muhidayat03@gmail.com>
2021-03-02 01:37:00 +0700

    fix error

--- src/pages/home/YoutubeSection.js
+++ src/pages/home/YoutubeSection.js
@@ -1,22 +1,25 @@
 import Youtube from "../../components/Youtube";
 import styled from "styled-components";
 
-const YoutubeSection = () =>
+const YoutubeSection = () => (
   <Section>
     <YoutubeContainer>
-      <Youtube src={"https://www.youtube.com/embed/8ASnyL_fZxQ?&autoplay=1&mute=1&enablejsapi=1&loop=1&playlist=8ASnyL_fZxQ"} />
+      <Youtube
+        src={
+          "https://www.youtube.com/embed/8ASnyL_fZxQ?&autoplay=1&mute=1&enablejsapi=1&loop=1&playlist=8ASnyL_fZxQ"
+        }
+      />
     </YoutubeContainer>
   </Section>
 );
 
 export default YoutubeSection;
 
-
 const Section = styled.div`
-  width: 100%;  
+  width: 100%;
   background-color: black;
-`
+`;
 const YoutubeContainer = styled.div`
   max-width: 1280px;
-  margin: auto;  
-`
+  margin: auto;
+`;
